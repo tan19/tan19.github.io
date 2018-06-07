d6683bfcc0fa1f1b98c4d66b56419bd0c2868bc1
Xi Tan <tan19@MacBook-Pro-2.fios-router.home>
2018-06-07 08:41:27 -0400

    update

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #sh
 ./jemdoc -c mysite.conf `find . -name "*.jemdoc"`
-#for file in `find . -name "*.pdf"`; do pdf2htmlEX --dest-dir `dirname $file` --fit-width 1024 $file; done
+for file in `find . -name "*.pdf"`; do pdf2htmlEX --dest-dir `dirname $file` --fit-width 1024 $file; done
